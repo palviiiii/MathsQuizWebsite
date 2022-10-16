cea042672286e8fd1b8494884b78035a428c8638
Palvi Deshpande <115910099+palviiiii@users.noreply.github.com>
2022-10-16 14:05:47 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The line segments of polygons are called?",
+    answer: "Sides",
+    options: [
+      "Vertices",
+      "Edges",
+      "Sides",
+      "Faces"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Polygons have at least 3 sides and?",
+    answer: "3 Angles",
+    options: [
+      "4 Angles",
+      "3 Angles",
+      "2 Angles",
+      "3 Faces"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Open-sided figures and shapes with curves are examples of?",
+    answer: "Non-Polygons",
+    options: [
+      "Polygons",
+      "Non-Polygons",
+      "2-Dimensional Figures",
+      "Quadrilateral"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Polygons can be mainly categorized as?",
+    answer: "Simple and Complex",
+    options: [
+      "Simple and Complex",
+      "Regular and Irregular",
+      "Concave and Convex",
+      "None of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "All irregular polygons fall under the category of?",
+    answer: "Complex",
+    options: [
+      "Simple",
+      "Complex",
+      "Concave",
+      "Convex"
+    ]
+  },
+];
